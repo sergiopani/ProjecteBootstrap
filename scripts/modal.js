dc3db6069d88f7eb8b897a256400b6716e27aa0a
Sergio Paniagua <sergiopani18>
2022-06-22 18:50:40 +0200

    Version Final 1

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,72 +1,75 @@
-/*Guardamos en cada variable cada 
-producto y despues su descripcion*/
 
-// onclick="${productos.setPrecio(precio)}"
-
-/*****************CODIGO ----- VUE****************/
-//import Formulario from "./formulario.vue";
-
-const app = Vue.createApp({
-  setup() {
-    const productos = [];
-    const cantidadTotal = 0;
-    const precioTotal = 0;
-    const modalWrap = null;
+new Vue({
+  el: "#app",
+  data: {
+    productos: [],
+    cantidadTotal: 0.0,
+    precioTotal: 0.0,
+    modalWrap: null,
+    actualPosition:1,//Inicializado a 1 
+    cantidadActual:0.0,   
     /*components: {
       Formulario,
     },*/
-    return {
-      productos,
-      cantidadTotal,
-      precioTotal,
-      modalWrap,
-    };
+
   },
+
   methods: {
-    abrirModal: function (url, descripcion, precio) {
-      //Llama a la funcion modal de Modal.js que mustra el modal
-      this.showModal(url, descripcion, precio);
+    abrirModal: function (actualPosition) {
+      //Pasamos la posicion actual del array
+      this.actualPosition = actualPosition;
+      var x = document.getElementById("modale");
+      if (x.style.display != "block") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+
+
+    },
+    cerrarModal: function(){
+      var x = document.getElementById("modale");
+      if (x.style.display != "block") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
     },
     setPrecio: function (precio) {
-      this.precioTotal += precio;
-      alert("El precio total es" + this.precioTotal);
+      precio *= this.cantidadActual;
+      this.precioTotal += precio;      
     },
 
-    setCantidad: function (cantidad) {
-      this.cantidadTotal += cantidad;
+    setCantidad: function () {
+      this.cantidadTotal += Number(this.cantidadActual);
+      this.cantidadActual = 0;
+      console.log("La cantidad es de " + this.cantidadTotal);
     },
 
-    showModal: function (url, descripcion, precio) {
-      //para no crear multiple boxes
-      if (modalWrap != null) {
-        modalWrap.remove();
+    validarCantidad: function (){
+      if(this.cantidadActual <= 0){
+        alert("La cantidad debe de ser mayor o igual a 0!");
       }
-      modalWrap = document.createElement("div");
-      modalWrap.innerHTML = `
-    <div class="modal" tabindex="-1">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header pb-0" style="border:none;">                  
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>          
-        </div>
-        <div class="modal-body pt-2 justify-content-md-center" style="border:none;text-align:center;">
-          <img src="${url}" class="col-12 px-7" style="width:400px;height:300px;" alt="${descripcion}">
-          <h5 class="pt-2">${descripcion}</h5>
-          <h6>${precio}€</h6>
-        </div>
-        <div class="modal-footer justify-content-md-center">
-            
-        </div>
-      </div>
-    </div>
-  </div>      
-  `;
-      //Provided by bootstrap
-      document.body.append(modalWrap);
-      var modal = new bootstrap.Modal(modalWrap.querySelector(".modal"));
-      modal.show();
+    }
+    /*
+    showModal: function (url, descripcion, precio) {
+        //para no crear multiple boxes
+        if (this.modalWrap != null) {
+            this.modalWrap.remove();
+        }
+        this.modalWrap = document.createElement("div");
+        this.modalWrap.innerHTML = `
+                
+            `;
+        //Provided by bootstrap
+        document.body.append(this.modalWrap);
+        var modal = new bootstrap.Modal(this.modalWrap.querySelector(".modal"));
+        modal.show();
     },
+    */
   },
+
+
 
   mounted() {
     fetch("http://localhost:3000/productos")
@@ -74,17 +77,5 @@ const app = Vue.createApp({
       .then((data) => (this.productos = data))
       .catch((err) => console.log(err.message));
   },
+
 });
-
-/*
-const enviarPrecio = (precio) => {
-  alert(precio);
-  return precio; 
-
-};
-
-const enviarCantidad = (cantidad) => {
-  return cantidad;
-};
-
-*/
