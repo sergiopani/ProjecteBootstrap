62868b4363114b249611bc10d037c7bb4849cf78
sergiopani <xpanisergio@gmail.com>
2022-06-22 12:59:44 +0200

    Estructure changed

---
+++ scripts/formulario.vue
@@ -0,0 +1,13 @@
+<template>
+  <form>
+    <label for="quantity"></label>
+    <input type="number" id="quantity" name="quantity" min="1" />
+    <input type="submit" class="btn btn-primary bg-dark col-6" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Formulario",
+};
+</script>
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,20 +1,53 @@
 /*Guardamos en cada variable cada 
 producto y despues su descripcion*/
-var modalWrap = null;
-const showModal = (url, descripcion, precio) => {
-  //para no crear multiple boxes
-  if (modalWrap != null) {
-    modalWrap.remove();
-  }
-  modalWrap = document.createElement("div");
-  modalWrap.innerHTML = `
+
+// onclick="${productos.setPrecio(precio)}"
+
+/*****************CODIGO ----- VUE****************/
+//import Formulario from "./formulario.vue";
+
+const app = Vue.createApp({
+  setup() {
+    const productos = [];
+    const cantidadTotal = 0;
+    const precioTotal = 0;
+    const modalWrap = null;
+    /*components: {
+      Formulario,
+    },*/
+    return {
+      productos,
+      cantidadTotal,
+      precioTotal,
+      modalWrap,
+    };
+  },
+  methods: {
+    abrirModal: function (url, descripcion, precio) {
+      //Llama a la funcion modal de Modal.js que mustra el modal
+      this.showModal(url, descripcion, precio);
+    },
+    setPrecio: function (precio) {
+      this.precioTotal += precio;
+      alert("El precio total es" + this.precioTotal);
+    },
+
+    setCantidad: function (cantidad) {
+      this.cantidadTotal += cantidad;
+    },
+
+    showModal: function (url, descripcion, precio) {
+      //para no crear multiple boxes
+      if (modalWrap != null) {
+        modalWrap.remove();
+      }
+      modalWrap = document.createElement("div");
+      modalWrap.innerHTML = `
     <div class="modal" tabindex="-1">
     <div class="modal-dialog">
       <div class="modal-content">
-        <div class="modal-header pb-0" style="border:none;">
-                  
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          
+        <div class="modal-header pb-0" style="border:none;">                  
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>          
         </div>
         <div class="modal-body pt-2 justify-content-md-center" style="border:none;text-align:center;">
           <img src="${url}" class="col-12 px-7" style="width:400px;height:300px;" alt="${descripcion}">
@@ -22,15 +55,36 @@ const showModal = (url, descripcion, precio) => {
           <h6>${precio}€</h6>
         </div>
         <div class="modal-footer justify-content-md-center">
-          <input class="col-2" type="number" id="quantity" name="quantity" min="1" max="5">
-          <button type="button" class="btn btn-primary bg-dark col-6">COMPRAR</button>
+            
         </div>
       </div>
     </div>
   </div>      
   `;
-  //Provided by bootstrap
-  document.body.append(modalWrap);
-  var modal = new bootstrap.Modal(modalWrap.querySelector(".modal"));
-  modal.show();
+      //Provided by bootstrap
+      document.body.append(modalWrap);
+      var modal = new bootstrap.Modal(modalWrap.querySelector(".modal"));
+      modal.show();
+    },
+  },
+
+  mounted() {
+    fetch("http://localhost:3000/productos")
+      .then((res) => res.json())
+      .then((data) => (this.productos = data))
+      .catch((err) => console.log(err.message));
+  },
+});
+
+/*
+const enviarPrecio = (precio) => {
+  alert(precio);
+  return precio; 
+
 };
+
+const enviarCantidad = (cantidad) => {
+  return cantidad;
+};
+
+*/
