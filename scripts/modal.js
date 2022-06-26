1f5923fa3a4267cc228301572a3e0e2a75179d1d
Sergio Paniagua <sergiopani18>
2022-06-26 13:13:13 +0200

    Back end and Padding added

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,19 +1,22 @@
+//const { getProduct } = require("../../../controllers/product");
 
 new Vue({
   el: "#app",
-  data: {
-    productos: [],
+  data: {    
+    productos : [],
     cantidadTotal: 0.0,
     precioTotal: 0.0,
     modalWrap: null,
-    actualPosition:1,//Inicializado a 1 
-    cantidadActual:0.0,   
+    actualPosition: 1,//Inicializado a 1 
+    cantidadActual: 0.0,
     /*components: {
       Formulario,
     },*/
 
   },
-
+  created(){
+    this.getProduct();
+  },
   methods: {
     abrirModal: function (actualPosition) {
       //Pasamos la posicion actual del array
@@ -27,7 +30,7 @@ new Vue({
 
 
     },
-    cerrarModal: function(){
+    cerrarModal: function () {
       var x = document.getElementById("modale");
       if (x.style.display != "block") {
         x.style.display = "block";
@@ -37,7 +40,7 @@ new Vue({
     },
     setPrecio: function (precio) {
       precio *= this.cantidadActual;
-      this.precioTotal += precio;      
+      this.precioTotal += precio;
     },
 
     setCantidad: function () {
@@ -46,11 +49,11 @@ new Vue({
       console.log("La cantidad es de " + this.cantidadTotal);
     },
 
-    validarCantidad: function (){
-      if(this.cantidadActual <= 0){
+    validarCantidad: function () {
+      if (this.cantidadActual <= 0) {
         alert("La cantidad debe de ser mayor o igual a 0!");
       }
-    }
+    },
     /*
     showModal: function (url, descripcion, precio) {
         //para no crear multiple boxes
@@ -67,15 +70,29 @@ new Vue({
         modal.show();
     },
     */
+    
+    getProduct(){
+      fetch('/product/62b82018432ed755ec5fa6f4')
+      .then(res=>res.json())
+      .then(data=> {
+        
+        this.productos = data.product.productos;
+        console.log(this.productos);
+      })
+      .catch((err) => console.log(err.message));
+    }
   },
+  
 
-
-
+  /*
   mounted() {
-    fetch("http://localhost:3000/productos")
+    fetch('../data/pro')
       .then((res) => res.json())
+      .then(console.log(data))
       .then((data) => (this.productos = data))
       .catch((err) => console.log(err.message));
   },
+  */
+  
 
 });
